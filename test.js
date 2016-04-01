02938730f6b4bc399117442d0bd0d37c31881ea2
Kevin Ring <kevin@kotachrome.com>
2016-04-01 22:48:57 +1100

    Add test case.

---
+++ test.js
@@ -0,0 +1,13 @@
+function Test(options) {
+    console.log('start');
+
+    /**
+     * This is a jsdoc comment
+     */
+    this.foo = 'something';
+
+    /**
+     * Another comment
+     */
+    this.bar = 'something else';
+};
